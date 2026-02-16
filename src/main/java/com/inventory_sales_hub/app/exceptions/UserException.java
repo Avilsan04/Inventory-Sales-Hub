adb891c0e3f8fbadd0863c1b920bf7952a8e4ce5
Avilsan04 <alvaro.villalba.sanchez@bootcamp-upgrade.com>
2026-02-16 14:42:49 +0100

    Subida inicial

---
+++ src/main/java/com/inventory_sales_hub/app/exceptions/UserException.java
@@ -0,0 +1,8 @@
+package com.inventory_sales_hub.app.exceptions;
+
+public class UserException extends RuntimeException
+{
+    public UserException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/inventory_sales_hub/app/model/entities/User.java
@@ -0,0 +1,20 @@
+package com.inventory_sales_hub.app.model.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(unique = true, nullable = false)
+    private String email;
+
+}
